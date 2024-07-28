76514582e431c945d2936e150983ae891952024a
vugar-gasimov <vuqar5qasimov@gmail.com>
2024-07-28 19:32:10 +0300

    Improving server to get admin login info.

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 require('dotenv').config();
+
+app.use(
+  cors({
+    origin: ['http://localhost:3000'],
+    credentials: true,
+  })
+);
+
+app.use(bodyParser.json());
+app.use(cookieParser());
 
 app.use('/api', require('./routes/authRoutes'));
 
