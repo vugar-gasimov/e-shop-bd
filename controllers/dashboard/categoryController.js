4d4c5347d9a8a66c5fb39c9236291ea6567ef058
vugar-gasimov <vuqar5qasimov@gmail.com>
2024-08-21 17:04:57 +0400

    Category controller  with cloudinary image upload and creatimg module saving data.

--- controllers/dashboard/categoryController.js
+++ controllers/dashboard/categoryController.js
@@ -1,16 +1,52 @@
 const { formidable } = require('formidable');
 const { responseReturn } = require('../../utils/response');
+const cloudinary = require('cloudinary').v2;
+const categoryModel = require('../../models/categoryModel');
 class categoryController {
   addCategory = async (req, res) => {
     const form = formidable();
+
     form.parse(req, async (err, fields, files) => {
       if (err) {
         responseReturn(res, 404, { error: 'Something went wrong 404.' });
       } else {
         let { name } = fields;
         let { image } = files;
+
         name = name.trim();
         const slug = name.split('').join('-');
+
+        cloudinary.config({
+          cloud_name: process.env.CLOUD_NAME,
+          api_key: process.env.CLOUD_API_KEY,
+          api_secret: process.env.CLOUD_API_SECRET,
+          secure: true,
+        });
+        try {
+          const result = await cloudinary.uploader.upload(image.filepath, {
+            folder: 'categories',
+          });
+
+          if (result) {
+            const category = await categoryModel.create({
+              name,
+              image: result.url,
+              slug,
+            });
+            responseReturn(res, 201, {
+              category,
+              message: 'Image successfully uploaded 201.',
+            });
+          } else {
+            responseReturn(res, 404, {
+              error: 'Image upload failed 404.',
+            });
+          }
+        } catch (error) {
+          responseReturn(res, 500, {
+            error: 'Internal server error during image upload 500.',
+          });
+        }
       }
     });
   };
