9965dbe19c2e5ce9b7a4d931d6fb7675c2d7683c
vugar-gasimov <vuqar5qasimov@gmail.com>
2024-08-27 11:33:55 +0400

    Product controller add product logic update (cloudinary logic, geting data, making change in data and uploding images with loop).

--- controllers/dashboard/productController.js
+++ controllers/dashboard/productController.js
@@ -1,16 +1,45 @@
 const { formidable } = require('formidable');
 const { responseReturn } = require('../../utils/response');
 const cloudinary = require('cloudinary').v2;
-
+const productModel = require('../../models/productModel');
 class productController {
   add_product = async (req, res) => {
     const form = formidable({ multiples: true });
 
     form.parse(req, async (err, field, files) => {
-      console.log(files.images[0]);
-      console.log(field);
+      let {
+        name,
+        category,
+        description,
+        stock,
+        price,
+        discount,
+        shopName,
+        brand,
+      } = field;
+      const { images } = files;
+      name = name.trim();
+      const slug = name.split(' ').join('-');
+
+      cloudinary.config({
+        cloud_name: process.env.CLOUD_NAME,
+        api_key: process.env.CLOUD_API_KEY,
+        api_secret: process.env.CLOUD_API_SECRET,
+        secure: true,
+      });
+
+      try {
+        let allImageUrls = [];
+        for (let i = 0; i < images.length; i++) {
+          const result = await cloudinary.uploader.upload(images[i].filepath, {
+            folder: 'products',
+          });
+          allImageUrls = [...allImageUrls, result.url];
+        }
+        await productModel.create;
+      } catch (error) {}
     });
-  };
+  }; // End of add product method
 }
 
 module.exports = new productController();
