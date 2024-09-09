a4f713d9ce09012651e8093efda148d22146b805
vugar-gasimov <vuqar5qasimov@gmail.com>
2024-09-09 22:34:53 +0400

    New controller to get products, method is complete, plus it's route.

--- controllers/home/homeControllers.js
+++ controllers/home/homeControllers.js
@@ -1,13 +1,32 @@
 const categoryModel = require('../../models/categoryModel');
+const productModel = require('../../models/productModel');
 const { responseReturn } = require('../../utils/response');
 
 class homeControllers {
+  formateProducts = (products) => {
+    const productArray = [];
+    let i = 0;
+    while (i < products.length) {
+      let temp = [];
+      let j = i;
+      while (j < i + 3) {
+        if (products[j]) {
+          temp.push(products[j]);
+        }
+        j++;
+      }
+      productArray.push([...temp]);
+      i = j;
+    }
+    return productArray;
+  };
+
   getCategories = async (req, res) => {
     try {
       const categories = await categoryModel.find({});
       responseReturn(res, 200, {
+        categories,
         success: true,
-        data: categories,
         message: 'Categories fetched successfully',
       });
     } catch (error) {
@@ -19,7 +38,51 @@ class homeControllers {
       });
     }
   };
+  // End of get categories method
+
+  getProducts = async (req, res) => {
+    try {
+      const products = await productModel
+        .find({})
+        .limit(12)
+        .sort({ createdAt: -1 });
+
+      const allProducts1 = await productModel
+        .find({})
+        .limit(9)
+        .sort({ createdAt: -1 });
+      const latest_products = this.formateProducts(allProducts1);
+
+      const allProducts2 = await productModel
+        .find({})
+        .limit(9)
+        .sort({ rating: -1 });
+      const topRated_products = this.formateProducts(allProducts2);
+
+      const allProducts3 = await productModel
+        .find({})
+        .limit(9)
+        .sort({ discount: -1 });
+      const discounted_products = this.formateProducts(allProducts3);
+
+      responseReturn(res, 200, {
+        products,
+        latest_products,
+        topRated_products,
+        discounted_products,
+        success: true,
+        message: 'Products fetched successfully',
+      });
+    } catch (error) {
+      console.error('Error fetching products:', error);
+
+      responseReturn(res, 500, {
+        success: false,
+        message: 'Failed to fetch products',
+      });
+    }
+  };
+  // End of get products method
 }
-// End of get categories method
 
 module.exports = new homeControllers();
--- routes/home/homeRoutes.js
+++ routes/home/homeRoutes.js
@@ -2,5 +2,6 @@ const homeControllers = require('../../controllers/home/homeControllers');
 const router = require('express').Router();
 
 router.get('/get-categories', homeControllers.getCategories);
+router.get('/get-products', homeControllers.getProducts);
 
 module.exports = router;
