377c3f6db5f0144000d373c00ef74f5915889989
vugar-gasimov <vuqar5qasimov@gmail.com>
2024-11-01 21:14:46 +0400

    Payment comtroller new method get create stripe connect accout.
    Backend module to manage Stripe Connect accounts for vendors. It creates or refreshes vendor accounts, generates unique onboarding links, and provides success or error messages based on the operation’s outcome.

--- controllers/payment/paymentController.js
+++ controllers/payment/paymentController.js
@@ -7,15 +7,56 @@ const { responseReturn } = require('../../utils/response');
 
 class paymentController {
   create_stripe_connect_account = async (req, res) => {
-    // const {} = req.body;
-    // try {
-    //   const admin = await adminModel.findOne({ email }).select('+password');
-    //   responseReturn(res, 200, {
-    //     message: '',
-    //   });
-    // } catch (error) {
-    //   console.log(error.message);
-    // }
+    const { id } = req;
+    const uId = uuidv4();
+    try {
+      const stripeInfo = await stripeModel.findOne({ vendorId: id });
+      if (stripeInfo) {
+        await stripeModel.deleteOne({ vendorId: id });
+        const account = await stripe.accounts.create({ type: 'express' });
+
+        const accountLink = await stripe.accountLinks.create({
+          account: account.id,
+          refresh_url: 'http://localhost:3001/refresh',
+          return_url: `http://localhost:3001/success?activeCode=${uId}`,
+          type: 'account_onboarding',
+        });
+        await stripeModel.create({
+          vendorId: id,
+          stripeId: account.id,
+          code: uId,
+        });
+        responseReturn(res, 200, {
+          url: accountLink.url,
+          message:
+            'Stripe Connect account created successfully. Please complete onboarding using the provided link.',
+        });
+      } else {
+        const account = await stripe.accounts.create({ type: 'express' });
+
+        const accountLink = await stripe.accountLinks.create({
+          account: account.id,
+          refresh_url: 'http://localhost:3001/refresh',
+          return_url: `http://localhost:3001/success?activeCode=${uId}`,
+          type: 'account_onboarding',
+        });
+        await stripeModel.create({
+          vendorId: id,
+          stripeId: account.id,
+          code: uId,
+        });
+        responseReturn(res, 200, {
+          url: accountLink.url,
+          message:
+            'Stripe Connect account created successfully. Please complete onboarding using the provided link.',
+        });
+      }
+    } catch (error) {
+      console.log(
+        'Failed to create Stripe Connect account. Please try again or contact support.' +
+          error.message
+      );
+    }
   }; // End of create stripe connect account method
 }
 module.exports = new paymentController();
